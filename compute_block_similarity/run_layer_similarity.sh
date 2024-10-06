4b19b12a19ce297eb4ab46a39def84f2618e7839
Parag Ekbote <thecoolekbote189@gmail.com>
2024-10-06 10:38:09 +0000

    Initial Model Recipe.

--- compute_block_similarity/run_layer_similarity.sh
+++ compute_block_similarity/run_layer_similarity.sh
@@ -3,7 +3,7 @@
 # This Bash script runs the Python script with arguments
 
 # Run the Python script with command-line arguments
-python layer_similarity.py --model_path "mistralai/Mistral-7B-Instruct-v0.2" \
+python layer_similarity.py --model_path "argilla/notus-7b-v1" \
                       --dataset "arcee-ai/sec-data-mini" \
                       --dataset_column "text" \
                       --batch_size 8 \
